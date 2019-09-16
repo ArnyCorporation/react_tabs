0d8c40bdc4b3c06fd276380440f1c399d631ed66
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-16 08:43:38 +0300

    Updates

---
+++ precache-manifest.9c5e9c4313c2b0ea5ef07da02c7cad92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307cf05550d13d1d537d1aa36a47bb4c",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "bb27d3a033d6ca2b2354",
+    "url": "/react_tabs/static/css/main.0985e21b.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/react_tabs/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "bb27d3a033d6ca2b2354",
+    "url": "/react_tabs/static/js/main.9ecdeeac.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
